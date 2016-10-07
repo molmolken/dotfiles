5b9a136b7f089cdb6245ff1456f2a58e524f71eb
44mol <moriken3.14@gmail.com>
2016-10-07 20:21:08 +0900

    setupにvimdocを追加

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ ln -s ~/dotfiles/.screenrc ~/.screenrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore.global ~/.gitignore.global
+
+# vimdoc-ja
+ln -s ~/dotfiles/vimdoc-ja/doc/ ~/.vim/doc
+ln -s ~/dotfiles/vimdoc-ja/syntax/ ~/.vim/syntax
+
