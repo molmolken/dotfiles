ff8813ca52ed1553e0d3d349164e624ec18f3aa3
44mol <moriken3.14@gmail.com>
2016-10-07 23:00:53 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,35 @@
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.screenrc ~/.screenrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.dein.toml ~/.dein.toml
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore.global ~/.gitignore.global
+#!/bin/sh
+
+deploy_files() {
+    if [ ! -e ~/dotfiles/$1 ]; then
+        echo "Error!!! : ~/dotfiles/$1 is not exist.";
+        return;
+    fi
+
+    if [ ! -e ~/$1 ]; then
+        ln -s ~/dotfiles/$1 ~/$1;
+    else
+        echo "Exist : $1";
+    fi
+}
+
+deploy_files .zshrc
+deploy_files .screenrc
+deploy_files .vimrc
+deploy_files .dein.toml
+deploy_files .gitconfig
+deploy_files .gitignore.global
 
 # vimdoc-ja
-ln -s ~/dotfiles/vimdoc-ja/doc/ ~/.vim/doc
-ln -s ~/dotfiles/vimdoc-ja/syntax/ ~/.vim/syntax
+if [ ! -e ~/.vim/doc ]; then
+    ln -s ~/dotfiles/vimdoc-ja/doc/ ~/.vim/doc;
+else
+    echo "Exist : doc";
+fi
+
+if [ ! -e ~/.vim/syntax ]; then
+    ln -s ~/dotfiles/vimdoc-ja/syntax/ ~/.vim/syntax;
+else
+    echo "Exist : syntax";
+fi
 
